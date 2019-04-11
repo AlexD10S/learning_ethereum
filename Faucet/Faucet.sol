25cec9eb352ea58d73a7f50d2d356b9f687a191c
Alex <alexfraga10@gmail.com>
2019-04-12 00:16:54 +0200

    Faucet - Added constructor and self-destruction method

--- Faucet/Faucet.sol
+++ Faucet/Faucet.sol
@@ -2,9 +2,19 @@ pragma solidity ^0.5.5;
 
 contract Faucet {
 
+    address owner;
+
+    constructor() public{
+        //We set us, as the owner of the smart contract
+		owner = msg.sender;
+        //Difference of this msg.sender with the one in the function withdraw, is that
+        //this is in the constructor, so it only going to be executed in the contract 
+        //creation, and we will be the one who make the transaction
+	}
+
     function withdraw(uint withdraw_amount) public {
         //value in wei(0.1 eth)
-        require(withdraw_amount <= 100000000000000000);
+        require(withdraw_amount <= 0.1 ether);
         //If the withdraw function is called with a 
         //withdraw_amount greater than the amount, require will
         //cause the execution to stop and fail with an exception
@@ -16,6 +26,11 @@ contract Faucet {
         msg.sender.transfer(withdraw_amount);
         //transfer is a built-in function that transfers ether
         //from the current contract to the address of the sender.
+    }
+    //Method to delete the contract
+    function destroy() public {
+	    require(msg.sender == owner);
+	    selfdestruct(owner);
     }
 
     //Fallback or default function. Called if the transaction
